7d27cba79ada7a57098cfec95364a8d579b726fa
amitsnh8 <amitsnh8@gmail.com>
2024-01-08 16:13:50 +0530

    n

--- src/App.js
+++ src/App.js
@@ -6,8 +6,15 @@ function App() {
     const handleAdd=()=>{
         setRow([...row,...inputArr])
     }
-    
-    
+    useEffect(()=>{
+      const fetchData=async ()=>{
+     const resp= await fetch('https://jsonplaceholder.typicode.com/todos/1');
+      console.log(resp);
+      
+      console.log("hi");
+    }
+    fetchData();
+    },[])
     return(
         <>
         {
